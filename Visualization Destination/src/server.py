68ba36fb045d03f04c37e791cba0ba312af3a8e2
Duarte Alves <Duarte's MBP>
2020-09-11 15:47:05 +0100

    Simple Pipeline Visualization

--- Visualization Destination/src/server.py
+++ Visualization Destination/src/server.py
@@ -1,11 +1,14 @@
 import grpc
 import io
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+import numpy as np
 import logging
 import sys
 
 
 from concurrent import futures
+from multiprocessing import Process, Queue
 from PIL import Image as PILImage
 from pipeline.core.connections.grpc.service_pb2 import DataTransferResponse
 from pipeline.core.connections.grpc import service_pb2_grpc
@@ -18,20 +21,13 @@ _MAX_WORKERS = 10
 
 class ServerImpl(service_pb2_grpc.DataTransferServiceServicer):
 
+    def __init__(self, results_queue: 'Queue'):
+        self.__results_queue = results_queue
+
     def Transfer(self, request_iterator, context):
         print("Received Transfer Request")
-        for request in request_iterator:
-            print(request.request_id)
-            request_metadata = request.metadata
-            request_image = Image()
-            request_metadata.Unpack(request_image)
-            image_bytes = request_image.bytes
-            image = PILImage.open(io.BytesIO(image_bytes))
-            image.show()
-            request_payload = request.payload
-            predict_response = PredictResponse()
-            request_payload.Unpack(predict_response)
-            print(predict_response)
+        for result in request_iterator:
+            self.__results_queue.put(result)
         return [DataTransferResponse()]
 
 
@@ -43,14 +39,51 @@ def parse_argv():
     return int(argv[1])
 
 
+def visualize_results(results_queue: 'Queue'):
+    """
+    Function to display the received results using matplotlib
+    :return:
+    """
+
+    # fig = plt.figure()
+    # ax = fig.gca()
+    fig, (ax1, ax2) = plt.subplots(ncols=2)
+
+    last_image = np.zeros((100, 100))
+
+    im = ax1.imshow(last_image)
+    text = ax2.text(0, 0, 'Initial Text')
+    while True:
+        result = results_queue.get()
+        if not result:
+            break
+
+        predict_response = PredictResponse()
+        result.payload.Unpack(predict_response)
+        print(predict_response)
+        text.set_text(predict_response.__repr__())
+
+        result_image = Image()
+        result.metadata.Unpack(result_image)
+        im.set_data(PILImage.open(io.BytesIO(result_image.bytes)))
+        print("On draw")
+        plt.draw()
+        print("After draw")
+        plt.pause(1e-3)
+
+
 def main():
     port = parse_argv()
+    results_queue = Queue()
+    visualization_process = Process(target=visualize_results, args=(results_queue,))
+    visualization_process.start()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=_MAX_WORKERS))
     service_pb2_grpc.add_DataTransferServiceServicer_to_server(
-        ServerImpl(), server)
+        ServerImpl(results_queue), server)
     server.add_insecure_port(f'[::]:{port}')
     server.start()
     server.wait_for_termination()
+    visualization_process.join()
 
 
 if __name__ == '__main__':
