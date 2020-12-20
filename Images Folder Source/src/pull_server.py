26ec31f12fd053a21fc7cccd105a43426c0e312f
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2020-12-20 11:32:21 +0000

    Server Streaming Pipeline

--- Images Folder Source/src/pull_server.py
+++ Images Folder Source/src/pull_server.py
@@ -15,9 +15,7 @@ class PullServer(sources_grpc.ImageDataSourceServicer):
     def Get(self, request, context):
         image_path = None
         while True:
-            directory_iter = filter(
-                    lambda x: x.is_file() and x.suffix in {'.jpg', '.png'},
-                    self.__image_dir.iterdir())
+            directory_iter = self.__get_directory_images_iter()
             while not image_path:
                 try:
                     image_path = next(directory_iter, None)
@@ -27,10 +25,28 @@ class PullServer(sources_grpc.ImageDataSourceServicer):
             if image_path:
                 break
         time.sleep(1)
+        return self.__get_response_from_path(image_path)
+
+    def GetStream(self, request, context):
+        directory_iter = self.__get_directory_images_iter()
+        for image_path in directory_iter:
+            response = self.__get_response_from_path(image_path)
+            yield response
+            time.sleep(0.2)
+
+    def __get_directory_images_iter(self):
+        directory_iter = filter(
+            lambda x: x.is_file() and x.suffix in {'.jpg', '.png'},
+            self.__image_dir.iterdir())
+        return directory_iter
+
+    @staticmethod
+    def __get_response_from_path(image_path):
         with open(image_path, 'rb') as fp:
             image_bytes = fp.read()
             response = image.Image(bytes=image_bytes,
                                    format=''.join(image_path.suffixes),
                                    name=image_path.stem)
-
         return response
+
+
